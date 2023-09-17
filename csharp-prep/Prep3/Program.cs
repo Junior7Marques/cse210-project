c12549aa011ea49adbbb07edc73d69f6e110e212
Junior7Marques <juniorjr07@live.com>
2023-09-17 15:54:32 -0300

    Week2

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -4,6 +4,29 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep3 World!");
+        Random randomGenerator = new Random();
+        int number = randomGenerator.Next(1, 10);
+        int shoot = 0;
+        int trys = 0;
+        while (shoot != number)
+        {
+        Console.Write("Try to Guess the Number:  ");
+        shoot = int.Parse(Console.ReadLine());
+        if (shoot == number)
+        {
+            Console.Write("You guessed it!  ");
+            Console.Write($"You tried {trys} times. ");
+        }
+        if (shoot > number)
+        {
+            Console.Write("Lower! ");
+            trys += 1;
+        }
+        if (shoot < number)
+        {
+            Console.Write("Higher! ");
+            trys += 1;
+        }
+        }
     }
 }
